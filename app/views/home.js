7b94f8f713f22c1f364a00c131813b64d84b34a1
Saravanan Ramasamy <saravanan_ramasamy@trimble.com>
2021-05-29 15:41:17 +0530

    Material ui addition

--- app/views/home.js
+++ app/views/home.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import Button from '@material-ui/core/Button';
 
 class Home extends Component {
-  componentDidMount () {
+  componentDidMount() {
     this.props.dispatch(actions.getSettings());
     this.props.dispatch(actions.searchUsers());
   }
@@ -11,22 +12,25 @@ class Home extends Component {
     let renderSet = null;
     let searchUsers = this.props.searchUsers;
     console.log(searchUsers.success)
-    if(this.props && this.props.settings) {
+    if (this.props && this.props.settings) {
       renderSet = this.props.settings;
       console.log(renderSet.settings)
     }
-    
+
     return (
       <div>
         <h2>Home</h2>
-        {renderSet ? <p>{renderSet.settings}</p>: null}
-        {searchUsers ? <p>{searchUsers.success}</p>: null}
+        {renderSet ? <p>{renderSet.settings}</p> : null}
+        {searchUsers ? <p>{searchUsers.success}</p> : null}
+        <Button variant="contained" color="primary">
+          React Node REdux
+        </Button>
       </div>
     );
   }
 }
 
-function select (state) {
+function select(state) {
   return {
     settings: state.settings,
     searchUsers: state.searchUsers
